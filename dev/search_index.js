5861288036ee6324e1abcce9f93cfa30e7718f84
Documenter.jl <documenter@juliadocs.github.io>
2025-05-16 16:42:58 +0000

    build based on 976496a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NowcastingRepl.jl","page":"NowcastingRepl.jl","title":"NowcastingRepl.jl","text":"","category":"section"},{"location":"","page":"NowcastingRepl.jl","title":"NowcastingRepl.jl","text":"Documentation for NowcastingRepl.jl","category":"page"}]
+}
