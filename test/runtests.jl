9f3237fa8231fcf383492d8b2ee55064f444f42b
Valerio Palagi <valeriopalagi@gmail.com>
2025-05-20 11:36:20 +0200

    Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,20 @@
 using NowcastingRepl
-using Test
+using Test, CSV, DataFrames
+
+logistic(x) = 1 / (1 + exp(-x))
+data_us = CSV.read("./Data/Data_US.csv", DataFrame; dateformat="m/d/y H:M")
+data_ea = CSV.read("./Data/Data_EA.csv", DataFrame; dateformat="m/d/y H:M")
+
 
 @testset "NowcastingRepl.jl" begin
-    # Test the run function
-   # NowcastingRepl.run()
-    
-    # Check if the output file exists
-   # @test isfile("./Output/Figure1.png") == true
-    
-    # Check if the output file is not empty 
-   # @test filesize("./Output/Figure1.png") > 0
-    
-    # Check if the output file is a PNG
-    @test endswith("./Output/Figure1.png", ".png")
+    @testset "Basic logistic function tests" begin
+    @test isapprox(logistic(0), 0.5; atol=1e-6)
+    @test logistic(100) ≈ 1.0
+    @test logistic(-10000) ≈ 0.0
+end
+
+@testset "Recession series validity" begin
+        @test all(x -> x in (0, 1), data_us.NBER_Recession)
+        @test all(x -> x in (0, 1), data_ea.CEPR_Recession[62:end])
+end
 end
