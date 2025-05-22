bc4e7eee1924ee004d07441ea52b4181cea2ce20
ValerioPalagi <132820947+ValerioPalagi@users.noreply.github.com>
2025-05-22 17:46:42 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,31 @@
-using Documenter
-using NowcastingRepl
+#using Documenter
+#using NowcastingRepl
 
-makedocs(
-    sitename = "NowcastingRepl.jl",
-    modules = [NowcastingRepl],
-    format = Documenter.HTML(),
-    pages = [
+#makedocs(
+ #   sitename = "NowcastingRepl.jl",
+  #  modules = [NowcastingRepl],
+   # format = Documenter.HTML(),
+    #pages = [
+     #   "Home" => "index.md",
+    #],
+    #repo = "https://github.com/ValerioPalagi/NowcastingRepl.jl",
+    #authors = "Valerio Palagi, Isotta Valpreda"
+#)
+using NowcastingRepl
+using Documenter
+
+DocMeta.setdocmeta!(NowcastingRepl, :DocTestSetup, :(using NowcastingRepl); recursive=true)
+
+makedocs(;
+    modules=[NowcastingRepl],
+    authors="Valerio Palagi, Isotta Valpreda",
+    sitename="NowcastingRepl.jl",
+    format=Documenter.HTML(;
+        canonical="https://github.com/ValerioPalagi/NowcastingRepl.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
         "Home" => "index.md",
     ],
-    repo = "https://github.com/ValerioPalagi/NowcastingRepl.jl",
-    authors = "Valerio Palagi, Isotta Valpreda"
 )
